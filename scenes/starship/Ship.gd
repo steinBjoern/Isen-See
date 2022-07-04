d238fc9e555dfc33f18757d27bc65bb8a411c5a3
steinBjoern <steinbjoern@protonmail.com>
2022-07-04 18:13:51 +0200

    working on steer algorithm.

--- scenes/starship/Ship.gd
+++ scenes/starship/Ship.gd
@@ -10,7 +10,7 @@ var at : Dictionary = {
 var mv : Dictionary = {
 	# velocity
 	'force' : 900.0,
-	'angular_force' : 9000.0,
+	'angular_force' : 900000.0,
 	'direction' : Vector2.ZERO,
 }
 
@@ -48,24 +48,32 @@ func control_force( event : InputEvent):
 
 func steer_to_point( point : Vector2, state : Physics2DDirectBodyState):
 	steer( point, state)
+	
 
 
 func steer( point : Vector2, state : Physics2DDirectBodyState):
+	var delta = get_physics_process_delta_time()
 	var pos : Vector2 = global_position
 	var rot : float = global_rotation
-	var end_rot : float = pos.angle_to( point)
+	var end_rot : float = pos.angle_to_point( point)
+	var dr = wrapf(end_rot - rot, -PI, PI)
 	
-	if abs(end_rot - rot) > PI / 360:
-		var dr = wrapf(pos.angle_to_point( point) - global_rotation, -PI, PI)
+	if 0==0: # abs( dr) > PI / 360:
 		var spin_force = angular_velocity * inertia
-		
-		var stop_time = spin_force / mv.torque
-		var stop_way = mv.torque / inertia * pow( stop_time, 2)
+		var stop_time = abs( spin_force / mv.angular_force) # abs() still unneccessaru
+		var stop_way = mv.angular_force / inertia * pow( stop_time, 2)
 		
 		if abs( dr) > stop_way:
-			set_applied_torque( mv.torque * sign( dr))
+			set_applied_torque( mv.angular_force * sign( dr))
+			
+		elif abs( dr) > angular_velocity * delta:
+			set_applied_torque( mv.angular_force * -sign( angular_velocity))
+			
 		else:
-			set_applied_torque( mv.torque * -sign( angular_velocity))
+			set_applied_torque( 0)
+			state.set_angular_velocity( .0)
+			state.set_transform( state.transform.rotated( dr))
+			print( dr)
 			
 		
 	
@@ -78,6 +86,7 @@ func move_to_point( point : Vector2, state : Physics2DDirectBodyState):
 
 func get_mv() -> Dictionary:
 	return mv
+	
 
 
-
+# Copyright © 2022 Björn Moritz Sawatzky | License: MIT
