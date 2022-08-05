953cb67414cc10026bc3bb9b042d16166ba28475
Bjoern Moritz Sawatzky <42405268+steinBjoern@users.noreply.github.com>
2022-08-05 16:06:01 +0200

    renaming with the web ui, an adventura.

---
+++ scenes/actor/craft/Craft.gd
@@ -0,0 +1,34 @@
+extends Mover
+
+# attributes
+const default_at : Dictionary = {
+	'hitpoints' : .0,
+	'max_hp' : .0,
+}
+
+var at : Dictionary = default_at
+
+func _ready():
+	add_to_group( 'avatar')
+	print( 'hello ship ', self.name)
+	
+
+
+func _integrate_forces( state):
+	fly( state)
+	steer()
+	
+
+
+func integrate_parts():
+	at = default_at
+	for c in get_children():
+		if c.has_method( 'get_at'):
+			at[ 'hitpoints'] += c.get_at()[ 'hitpoints']
+			at[ 'max_hp'] += c.get_at()[ 'max_hp']
+			
+		
+	print( at)
+
+
+# Copyright © 2022 Björn Moritz Sawatzky | License: MIT
